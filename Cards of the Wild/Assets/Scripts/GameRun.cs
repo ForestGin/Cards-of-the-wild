8083ddb700475de937f44e8812ce93022b05dac6
Pol Bosch <pol.hcsob@gmail.com>
2021-01-24 19:41:56 +0100

    Fixing previous commit

--- Cards of the Wild/Assets/Scripts/GameRun.cs
+++ Cards of the Wild/Assets/Scripts/GameRun.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 public class GameRun : MonoBehaviour
 {
 
-	// Management of sprites
-	private Object[] backgrounds;
-	private Object[] props;
-	private Object[] chars;
+    // Management of sprites
+    private Object[] backgrounds;
+    private Object[] props;
+    private Object[] chars;
 
-	// Game management
-	private GameObject enemyCards;
-	private int [] enemyChars;	
-	private Agent agent;
+    // Game management
+    private GameObject enemyCards;
+    private int[] enemyChars;
+    private Agent agent;
 
     private GameObject playerCards;
 
     private int NUM_ENEMY_CARDS = 3;
-	private int NUM_CLASSES     = 3;
-	private int DECK_SIZE       = 4;
+    private int NUM_CLASSES = 3;
+    private int DECK_SIZE = 4;
 
-	// Rewards
-	private float RWD_ACTION_INVALID = -2.0f;
-	private float RWD_HAND_LOST      = -1.0f;
-	private float RWD_TIE            = -0.1f;
-	private float RWD_HAND_WON       =  1.0f;
+    // Rewards
+    private float RWD_ACTION_INVALID = -2.0f;
+    private float RWD_HAND_LOST = -1.0f;
+    private float RWD_TIE = -0.1f;
+    private float RWD_HAND_WON = 1.0f;
 
-	// Other UI elements
-	private UnityEngine.UI.Text textDeck;
+    // Other UI elements
+    private UnityEngine.UI.Text textDeck;
     private UnityEngine.UI.Text textEnemyScore;
     private UnityEngine.UI.Text textPlayerScore;
     private UnityEngine.UI.Text textTiesScore;
@@ -50,8 +50,8 @@ public class GameRun : MonoBehaviour
 
         // Load all prefabs
         backgrounds = Resources.LoadAll("Backgrounds/");
-        props       = Resources.LoadAll("Props/");
-        chars       = Resources.LoadAll("Chars/");
+        props = Resources.LoadAll("Props/");
+        chars = Resources.LoadAll("Chars/");
 
 
         ///////////////////////////////////////
@@ -70,7 +70,7 @@ public class GameRun : MonoBehaviour
         enemyChars = new int[NUM_ENEMY_CARDS];
 
         playerCards = GameObject.Find("PlayerCards");
-        agent      = GameObject.Find("AgentManager").GetComponent<Agent>();
+        agent = GameObject.Find("AgentManager").GetComponent<Agent>();
 
         agent.Initialize();
 
@@ -83,12 +83,12 @@ public class GameRun : MonoBehaviour
         ///////////////////////////////////////
         // Image generation
         ///////////////////////////////////////
-    	//renderTexture = gameObject.GetComponent<Camera>().targetTexture;
+        //renderTexture = gameObject.GetComponent<Camera>().targetTexture;
 
-    	//imgWidth  = renderTexture.width;
-    	//imgHeight = renderTexture.height;
+        //imgWidth  = renderTexture.width;
+        //imgHeight = renderTexture.height;
 
-        
+
     }
 
 
@@ -97,24 +97,24 @@ public class GameRun : MonoBehaviour
     private int GenerateCard(Transform parent)
     {
 
-    	int idx = Random.Range(0, backgrounds.Length);
-    	Instantiate(backgrounds[idx], parent.position, Quaternion.identity, parent);
+        int idx = Random.Range(0, backgrounds.Length);
+        Instantiate(backgrounds[idx], parent.position, Quaternion.identity, parent);
 
 
-    	idx               = Random.Range(0, props.Length);
-    	Vector3 position = new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f), -1.0f);
-   	  	Instantiate(props[idx], parent.position+position, Quaternion.identity, parent);
+        idx = Random.Range(0, props.Length);
+        Vector3 position = new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f), -1.0f);
+        Instantiate(props[idx], parent.position + position, Quaternion.identity, parent);
 
-    	idx         = Random.Range(0, chars.Length);
-    	position    = new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f), -2.0f);    	
-   	  	Instantiate(chars[idx], parent.position+position, Quaternion.identity, parent);
+        idx = Random.Range(0, chars.Length);
+        position = new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f), -2.0f);
+        Instantiate(chars[idx], parent.position + position, Quaternion.identity, parent);
 
-   	  	// Determine label of the character, return it
-   	  	int label = 0;
-   	  	if(chars[idx].name.StartsWith("frog")) label = 1;
-   	  	else if(chars[idx].name.StartsWith("opposum")) label = 2;
+        // Determine label of the character, return it
+        int label = 0;
+        if (chars[idx].name.StartsWith("frog")) label = 1;
+        else if (chars[idx].name.StartsWith("opposum")) label = 2;
 
-    	return label;
+        return label;
     }
 
     private void GeneratePlayerCard(Transform parent, int charindex)
@@ -136,31 +136,34 @@ public class GameRun : MonoBehaviour
 
     // Generate another turn
     IEnumerator GenerateTurn()
-    {	
-    	for(int turn=0; turn<100000; turn++) {
+    {
+        for (int turn = 0; turn < 100000; turn++)
+        {
 
-	        ///////////////////////////////////////
-	        // Generate enemy cards
-	        ///////////////////////////////////////
+            ///////////////////////////////////////
+            // Generate enemy cards
+            ///////////////////////////////////////
 
-	    	// Destroy previous sprites (if any) and generate new cards
-	    	int c = 0;
-	    	foreach(Transform card in enemyCards.transform) {
-	    		foreach(Transform sprite in card) {
-	    			Destroy(sprite.gameObject);
-	    		}
+            // Destroy previous sprites (if any) and generate new cards
+            int c = 0;
+            foreach (Transform card in enemyCards.transform)
+            {
+                foreach (Transform sprite in card)
+                {
+                    Destroy(sprite.gameObject);
+                }
 
-	    		enemyChars[c++] = GenerateCard(card);
-	    	}
+                enemyChars[c++] = GenerateCard(card);
+            }
 
 
-	        ///////////////////////////////////////
-	        // Generate player deck
-	        ///////////////////////////////////////
-	        int [] deck   = GeneratePlayerDeck();
-	        textDeck.text = "Deck: ";
-	        foreach(int card in deck)
-	        	textDeck.text += card.ToString() + "/";
+            ///////////////////////////////////////
+            // Generate player deck
+            ///////////////////////////////////////
+            int[] deck = GeneratePlayerDeck();
+            textDeck.text = "Deck: ";
+            foreach (int card in deck)
+                textDeck.text += card.ToString() + "/";
 
             textEnemyScore.text = "Wins: " + EnemyScore.ToString();
             textPlayerScore.text = "Wins: " + PlayerScore.ToString();
@@ -175,11 +178,11 @@ public class GameRun : MonoBehaviour
             //            the newly generated cards (otherwise it will see the previous ones)
             yield return new WaitForEndOfFrame();
 
-	        int [] action = agent.Play(deck, enemyChars);
+            int[] action = agent.Play(deck, enemyChars);
 
-	        textDeck.text += " Action:";
-	        foreach(int a in action)
-	        	textDeck.text += a.ToString() + "/";
+            textDeck.text += " Action:";
+            foreach (int a in action)
+                textDeck.text += a.ToString() + "/";
 
 
             int player = 0;
@@ -199,70 +202,70 @@ public class GameRun : MonoBehaviour
             // Compute reward
             ///////////////////////////////////////
             float reward = ComputeReward(deck, action);
-	        
-	        Debug.Log("Turn/reward: " + turn.ToString() + "->" + reward.ToString());
 
-	        agent.GetReward(reward);
+            Debug.Log("Turn/reward: " + turn.ToString() + "->" + reward.ToString());
 
-
-	        ///////////////////////////////////////
-	        // Manage turns/games
-	        ///////////////////////////////////////
+            agent.GetReward(reward);
 
 
+            ///////////////////////////////////////
+            // Manage turns/games
+            ///////////////////////////////////////
 
-	    	yield return new WaitForSeconds(0.1f);
 
-    	}
+
+            yield return new WaitForSeconds(0.1f);
+
+        }
 
     }
 
 
     // Auxiliary methods
-    private int [] GeneratePlayerDeck()
+    private int[] GeneratePlayerDeck()
     {
-    	int [] deck = new int [DECK_SIZE];
+        int[] deck = new int[DECK_SIZE];
 
-    	for(int i=0; i<DECK_SIZE; i++)
-    	{
-    		deck[i] = Random.Range(0, NUM_CLASSES);  // high limit is exclusive so [0, NUM_CLASSES-1]
-    	}
+        for (int i = 0; i < DECK_SIZE; i++)
+        {
+            deck[i] = Random.Range(0, NUM_CLASSES);  // high limit is exclusive so [0, NUM_CLASSES-1]
+        }
 
-    	return deck;
+        return deck;
     }
 
     // Compute the result of the turn and return the associated reward 
     // given the cards selected by the agent (action)
-   	// deck -> array with the number of cards of each class the player has
-   	// action -> array with the class of each card played
-    private float ComputeReward(int [] deck, int [] action)
+    // deck -> array with the number of cards of each class the player has
+    // action -> array with the class of each card played
+    private float ComputeReward(int[] deck, int[] action)
     {
-    	// First check if the action is valid given the player's deck
-    	foreach(int card in action)
-    	{
-    		deck[card]--;
+        // First check if the action is valid given the player's deck
+        foreach (int card in action)
+        {
+            deck[card]--;
             if (deck[card] < 0)
             {
                 InvalidScore++;
                 return RWD_ACTION_INVALID;
             }
-    	}
+        }
 
 
-    	// Second see who wins
-    	int score = 0;
-    	for(int i=0; i<NUM_ENEMY_CARDS; i++)
-    	{
-    		if(action[i] != enemyChars[i])
-    		{
-    			if(action[i] > enemyChars[i] || action[i]==0 && enemyChars[i]==2)	
-    				score++;
-    			else
-    				score--;
-    		}
-    		
-    	}
-        
+        // Second see who wins
+        int score = 0;
+        for (int i = 0; i < NUM_ENEMY_CARDS; i++)
+        {
+            if (action[i] != enemyChars[i])
+            {
+                if (action[i] > enemyChars[i] || action[i] == 0 && enemyChars[i] == 2)
+                    score++;
+                else
+                    score--;
+            }
+
+        }
+
         if (score == 0)
         {
             TiesScore++;
